977fea6bd2dc085608ff96cc29da3fc469211c09
Dasha <girl.is.anime20@gmail.com>
2024-01-23 22:58:45 +0300

    feat: add 6 solution task and correct 5

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,57 +1,35 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 
-async function mergeStyle(){
-  try{
-    const pathStyles = path.join(__dirname, 'styles');
-    const pathProjects = path.join(__dirname, 'project-dist');
+const pathStyles = path.join(__dirname, 'styles');
+const pathProjects = path.join(__dirname, 'project-dist');
+
+
+fs.readdir(pathStyles, (err, files) => {
+  if (err){
+    console.log(err)
+  } else{
     const arrStyle = [];
-
-    await fs.mkdir(pathProjects, { recursive: true });
-    const files = await fs.readdir(pathStyles);
-
-    await Promise.all(files.map(async (file) => {
-      const stats = await fs.stat(path.join(pathStyles, file));
-      if (stats.isFile() && path.extname(file).toLowerCase() === '.css'){
-        const data = await fs.readFile(path.join(pathStyles, file), 'utf8')
-        arrStyle.push(data);
-      }
-    }));
-
-    const newArr = arrStyle.join('\n');
-    await fs.writeFile(path.join(pathProjects, 'bundle.css'), newArr, 'utf8');
-  } catch(err){
-    console.error('Error:', err.message);
+    files.forEach(e => {
+      fs.stat(pathStyles + `/${e}`, (err, stats) =>{
+        if (stats.isFile() && path.extname(e).toLowerCase() === '.css'){
+          fs.readFile(pathStyles + `/${e}`, 'utf8', (err, data) =>{
+            if (err) {
+              console.log(err)
+            } else {
+              arrStyle.push(data)
+              if (arrStyle.length === files.length - 1){
+                const newArr = arrStyle.join('\n')
+                fs.writeFile(`${pathProjects}/bundle.css`, newArr, 'utf8', (err) =>{
+                  if (err){
+                    console.log(err)
+                  }
+                })
+              }
+            }
+          })
+        }
+      })
+    });
   }
-}
-
-mergeStyle();
-
-// fs.readdir(pathStyles, (err, files) => {
-//   if (err){
-//     console.log(err)
-//   } else{
-//     const arrStyle = [];
-//     files.forEach(e => {
-//       fs.stat(pathStyles + `/${e}`, (err, stats) =>{
-//         if (stats.isFile() && path.extname(e).toLowerCase() === '.css'){
-//           fs.readFile(pathStyles + `/${e}`, 'utf8', (err, data) =>{
-//             if (err) {
-//               console.log(err)
-//             } else {
-//               arrStyle.push(data)
-//               if (arrStyle.length === files.length - 1){
-//                 const newArr = arrStyle.join('\n')
-//                 fs.writeFile(`${pathProjects}/bundle.css`, newArr, 'utf8', (err) =>{
-//                   if (err){
-//                     console.log(err)
-//                   }
-//                 })
-//               }
-//             }
-//           })
-//         }
-//       })
-//     });
-//   }
-// })
+})
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,6 +1,78 @@
 const fs = require('fs').promises;
+const { readdir } = require('fs');
 const path = require('path');
+const { start } = require('repl');
 
+const importDir = path.join(__dirname, 'project-dist');
+const rootDir = path.join(__dirname);
+const fileCompont = path.join(__dirname, 'components');
+const assetsPath = path.join(__dirname, 'assets');
+const importPathAssets = path.join(importDir, 'assets');
+
+async function createHTML(){
+  try {
+
+    await fs.mkdir(importDir, {recursive: true});
+
+    let data = await fs.readFile(path.join(rootDir, 'template.html'), 'utf8');
+    const files = await fs.readdir(fileCompont);
+
+    await Promise.all(files.map(async (file) => {
+      const filesPath = path.join(fileCompont, file);
+      const fileName = path.parse(file).name;
+      const newData = await fs.readFile(filesPath, 'utf8');
+      data = data.toString().replace(`{{${fileName}}}`, newData);
+    }));
+
+    await fs.writeFile(path.join(importDir, 'index.html'), data, 'utf8');
+
+  }
+  catch(err){
+    console.error('Error:', err.message);
+}
+}
+
+async function mergeCSS(){
+  const pathStyles = path.join(__dirname, 'styles');
+  const arrStyle = [];
+
+  const files = await fs.readdir(pathStyles);
+
+  await Promise.all(files.map(async (file) => {
+    const stats = await fs.stat(path.join(pathStyles, file));
+    if (stats.isFile() && path.extname(file).toLowerCase() === '.css'){
+      const data = await fs.readFile(path.join(pathStyles, file), 'utf8')
+      arrStyle.push(data);
+    }
+  }));
+
+  const newArr = arrStyle.join('\n');
+  await fs.writeFile(path.join(importDir, 'style.css'), newArr, 'utf8');
+}
+
+async function addAssets(mainPath, importPath){
+  await fs.mkdir(importPath);
+  const files =  await fs.readdir(mainPath);
+
+  await Promise.all(files.map(async (file) =>{
+    const startPath = path.join(mainPath, file);
+    const endPath = path.join(importPath, file);
+    const stats = await fs.stat(startPath);
+
+    if (stats.isFile()){
+      const data = await fs.readFile(startPath, 'utf8');
+      await fs.writeFile(endPath, data, 'utf8');
+
+    } else if (stats.isDirectory()) {
+      await addAssets(startPath, endPath);
+    }
+  }));
+}
+
+
+createHTML()
+mergeCSS()
+addAssets(assetsPath, importPathAssets)
 
 // const fileCompont = path.join(__dirname, 'components');
 // const pathStyle = path.join(__dirname, 'styles');
@@ -28,29 +100,28 @@ const path = require('path');
 // createHTML();
 
 
-
-// fs.readFile(pathImport + '/template.html', 'utf8', (err, data) =>{
-//   if (err){
-//     console.log(err);
-//   } else {
-//     const newData = data;
-//     fs.readdir(fileCompont, (err, file) => {
-//       if (err){
-//         console.log(err);
-//       } else {
-//         file.forEach(e =>{
-//           fs.readFile(fileCompont + `/${e}`, 'utf8', (err, dataFile) => {
-//             if (err){
-//               console.log(err);
-//             } else {
-//               const name = path.parse(e).name;
-//               newData.replace( `${name}`, dataFile)
-//               console.log(newData)
+//
+// fs.readFile(mainPath + '/template.html', 'utf8', (err, data)=>{
+//   if (err) {console.log(err)}
+//   else{
+//     const tData = data
+//     fs.readdir(componentPath, (err, files)=>{
+//       if (err) {console.log(err)}
+//       else {
+//         files.forEach(file => {
+//           fs.readFile(componentPath + `/${file}`, 'utf8', (err, dataComp) => {
+//             if (err) {console.log(err)}
+//             else {
+//               const fileName = path.parse(file).name
+//               newData = tData.replace(`{{${fileName}}}`, dataComp)
 //             }
 //           })
 //         })
-//         fs.writeFile()
+//         fs.writeFile(newDir + '/index.html', newData, 'utf8', (err)=>{
+//           if (err) {console.log(err)}
+//         })
 //       }
 //     })
 //   }
+//
 // })
